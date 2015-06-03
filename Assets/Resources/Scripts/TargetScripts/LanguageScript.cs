3f3b3f43664954d97cd0323cc568f2525b6201c5
Gaillard <t_of_43@hotmail.fr>
2015-06-03 14:45:18 +0200

    Add the arrow to target the language selected in the main menu

--- Assets/Resources/Scripts/TargetScripts/LanguageScript.cs
+++ Assets/Resources/Scripts/TargetScripts/LanguageScript.cs
@@ -4,9 +4,14 @@ using System.Collections;
 public class LanguageScript : UnlightedTarget {
 	
 	public string LanguageKey = "";
-	
+	public Transform FlagTransform;
+	public Transform _activationTransform;
+
+
 	override public void LaunchAction()
 	{
 		Localization.SetLanguage(LanguageKey);
+		FlagTransform.position = _activationTransform.position;
+		FlagTransform.rotation = _activationTransform.rotation;
 	}
 }
